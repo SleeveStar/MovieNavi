5c897e53cb4aba1a821c88bf8ccd801d35eb5f2e
ManyCircle <ekdnjs000@naver.com>
2026-02-25 12:24:40 +0900

    first commit

---
+++ movienavi/src/main/java/com/movie/movienavi/MovienaviApplication.java
@@ -0,0 +1,13 @@
+package com.movie.movienavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovienaviApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovienaviApplication.class, args);
+    }
+
+}
---
+++ movienavi/src/test/java/com/movie/movienavi/MovienaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.movienavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovienaviApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
